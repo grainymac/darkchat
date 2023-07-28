3edc1ea30ab59ef7d2f9722de217ed34dda8713f
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-07-28 07:54:08 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD_Q2bFqNQTbWBR8a7k8xfUkrLdu1ur_Kk",
+  authDomain: "darkchat-19b1f.firebaseapp.com",
+  projectId: "darkchat-19b1f",
+  storageBucket: "darkchat-19b1f.appspot.com",
+  messagingSenderId: "786739505167",
+  appId: "1:786739505167:web:fd248fae042444f5095b7a",
+  measurementId: "G-G8PW6LTW2K"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
+const App = () => {
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+      {user ? <ChatRoom /> : <SignIn />}
+    </section>
     </div>
   );
 }
