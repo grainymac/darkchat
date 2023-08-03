b8f660a7839cb8e7a19cdd25e1818b41c491b127
Ian McIntosh <imcintosh22@turing.edu>
2023-08-03 20:29:54 +0000

    revamp

---
+++ src/Components/App/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './App.css'
+import { Auth } from '../Auth/Auth'
+import Cookies from 'universal-cookie'
+const cookies = new Cookies()
+
+function App() {
+
+  const [isAuth, setIsAuth] = useState(cookies.get('auth-token'))
+  const [room, setRoom] = useState(null)
+
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {room ? (
+      <div>Chat</div>
+      ) : (
+      <div className='room'>
+        <label>Enter Room Name:</label>
+        <input />
+        <button>Enter Chat</button>
+      </div> 
+      )}
+    </div>
+  )
+}
+
+export default App
