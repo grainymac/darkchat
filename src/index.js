923bd07cc52315345ce96389da1ccc010d7ba85b
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-08-06 09:04:01 -0500

    restarted darkchat

--- src/index.js
+++ src/index.js
@@ -6,10 +6,4 @@ import App from './Components/App/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
-<<<<<<< HEAD
 );
-=======
-  </React.StrictMode>
-);
-
->>>>>>> b8f660a7839cb8e7a19cdd25e1818b41c491b127
