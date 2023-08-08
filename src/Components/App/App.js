43f7564d0cc7cd39450167eb5483d4698a760eff
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-08-07 22:21:52 -0500

    adding styling to chat

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,13 +3,24 @@ import './App.sass'
 import Auth from '../Auth/Auth'
 import Chat from '../Chat/Chat'
 import Cookies from 'universal-cookie'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../firebase-config'
+import logOut from '../../assets/logout.png'
+
 const cookies = new Cookies()
 
 const App = () => {
-  const [isAuth, setIsAuth] = useState(cookies.get('auth-token'))
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
   const [room, setRoom] = useState(null)
 
   const roomInputRef = useRef(null)
+
+  const signUserOut = async () => {
+    await signOut(auth)
+    cookies.remove("auth-token")
+    setIsAuth(false)
+    setRoom(null)
+  }
 
   if (!isAuth) {
     return (
@@ -20,17 +31,24 @@ const App = () => {
   }
 
   return (
-    <div>
-      {room ? (
-        <Chat room={room} />
-      ) : (
-        <div className='room'>
-          <label>Enter Room Name:</label>
-          <input ref={roomInputRef} />
-          <button onClick={() => setRoom(roomInputRef.current.value)}> Enter Chat</button>
+    <>
+      <div className='app'>
+        {room ? (
+          <Chat room={room} />
+        ) : (
+          <div className='room'>
+            <label>Enter Room Name:</label>
+            <input ref={roomInputRef} />
+            <button onClick={() => setRoom(roomInputRef.current.value)}> Enter Chat</button>
+          </div>
+        )}
+        <div className='sign-out'>
+          <button onClick={signUserOut}>
+            <img src={logOut} alt='logout' />
+          </button>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   )
 }
 
