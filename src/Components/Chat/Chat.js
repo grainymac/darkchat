eee8bfb6103fc12525ba0913ba091e1cad47dde1
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-08-07 21:43:54 -0500

    chat messages

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,31 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import './Chat.sass'
-import { addDoc, collection, serverTimestamp, onSnapshot, query, where } from 'firebase/firestore'
+import { addDoc, collection, serverTimestamp, onSnapshot, query, where, orderBy } from 'firebase/firestore'
 import { db, auth } from '../../firebase-config'
 
 const Chat = ({ room }) => {
-    const [newMessage, setNewMessage] = useState('')
     const [user, setUser] = useState(null)
+    const [newMessage, setNewMessage] = useState('')
     const [messages, setMessages] = useState([])
-
     const messagesRef = collection(db, 'messages')
 
     useEffect(() => {
-        const queryMessages = query(messagesRef, where("room", "==", room))
-        onSnapshot(queryMessages, (snapshot) => {
-            let messages = []
-            snapshot.forEach((doc) => {
-                messages.push({ ...doc.data(), id: doc.id })
-            })
-            setMessages(messages)
-        })
-
+        const queryMessages = query(messagesRef, where("room", "==", room), orderBy('createdAt'))
+        
         const unsubscribe = auth.onAuthStateChanged((user) => {
             if (user) {
                 setUser(user)
             } else {
                 setUser(null)
             }
+
+            onSnapshot(queryMessages, (snapshot) => {
+                let messages = []
+                snapshot.forEach((doc) => {
+                    messages.push({ ...doc.data(), id: doc.id })
+                })
+                setMessages(messages)
+            })
         })
 
         return () => unsubscribe()
@@ -37,7 +37,7 @@ const Chat = ({ room }) => {
         await addDoc(messagesRef, {
             text: newMessage,
             createdAt: serverTimestamp(),
-            user: user ? user.displayName : 'Anonymous',
+            user: user ? user.displayName : 'User:',
             room: room,
         })
         setNewMessage('')
@@ -45,7 +45,17 @@ const Chat = ({ room }) => {
 
     return (
         <div className='chat-app'>
-            <div> {messages.map((message) => <h1>{message.text}</h1>)} </div>
+            <div className='header'>
+                <h1>Welcome to: {room.toUpperCase()} </h1>
+            </div>
+            <div className='messages'> 
+                {messages.map((message) => (
+                    <div className='message' key={message.id}>
+                        <span className='user'>{message.user} </span>
+                        {message.text}
+                    </div>
+                    ))} 
+            </div>
             <form onSubmit={handleSubmit} className='new-message-form'>
                 <input onChange={(e) => setNewMessage(e.target.value)} className='new-message-input' placeholder='Type your message here' value={newMessage} />
                 <button type='submit' className='send-button'>Send</button>
